b08468c8114f2cc1b986072531ba540dd59f8516
Adrian Michałek <devmichalek@gmail.com>
2021-06-24 21:31:42 +0200

    Add stubs

---
+++ States/SEditor.cpp
@@ -0,0 +1,1 @@
+#include "SEditor.hpp"
--- States/SEditor.hpp
+++ States/SEditor.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include "SAbstract.hpp"
+#include "DWallEditor.hpp"
+#include "DCarEditor.hpp"
 
 namespace State
 {
 	class Editor : public Abstract
 	{
+		DWallEditor m_wallEditor;
+		DCarEditor m_carEditor;
+
 	public:
 		Editor(Editor&) = delete;
 
